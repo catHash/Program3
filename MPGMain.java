0c546478773689f069a051b65ced614ed3b3b91a
Anonymous <anonymous@Anonymouss-MacBook-Pro.local>
2017-06-26 18:58:51 -0500

    fixed loops

--- MPGMain.java
+++ MPGMain.java
@@ -1,29 +1,28 @@
 //Jessica Hadwiger Program 3 6/26/17
 import java.util.Scanner;
-public class MPGMain 
+public class MPGMain
 {
 
-	public static void main(String[] args) 
+	public static void main(String[] args)
 	{
-		Scanner keyboard = new Scanner (System.in);	
+		Scanner keyboard = new Scanner (System.in);
 		Mileage mileage = new Mileage();
 		Distance dist = new Distance();
 		Speed rate = new Speed();
-		
+
 		//variables
 		double miles, gallons, distance, speed, time;
-		int ID;
-		
-		//Menu
-		System.out.println("Enter 1 to Determine Miles-per-Gallon");
-		System.out.println("Enter 2 to Determine Distance");
-		System.out.println("Enter 3 to Determine Miles-per-Hour");
-		System.out.println("Enter 4 to Exit");
-		ID = keyboard.nextInt();
-		
+		int ID = 0;
+
 		while (ID != 4)
 		{
-			if (ID >=1 && ID < 4)
+			//Menu
+			System.out.println("Enter 1 to Determine Miles-per-Gallon");
+			System.out.println("Enter 2 to Determine Distance");
+			System.out.println("Enter 3 to Determine Miles-per-Hour");
+			System.out.println("Enter 4 to Exit");
+			ID = keyboard.nextInt();
+			if (ID >=1 && ID <= 4)
 			{
 				switch(ID)
 				{
@@ -33,7 +32,7 @@ public class MPGMain
 					mileage.setMiles (miles);
 					System.out.println("How many gallons were used? ");
 					gallons = keyboard.nextDouble();
-					mileage.setGallons (gallons); 
+					mileage.setGallons (gallons);
 					System.out.printf("Your MPG is: %.2f", mileage.getMPG ());
 					break;
 				case 2: //Distance (R*T) Information
@@ -55,16 +54,12 @@ public class MPGMain
 					System.out.printf("Your MPH is: %.2f", rate.getMPH());
 					break;
 				}//end of switch
+                System.out.println();
 			}//end of of if
 			else
 			{
 				System.out.println("The value entered is not a correct entry.");
 				System.out.println("Please enter a value between 1-3 or enter 4 to exit. ");
-				System.out.println("Enter 1 to Determine Miles-per-Gallon");
-				System.out.println("Enter 2 to Determine Distance");
-				System.out.println("Enter 3 to Determine Miles-per-Hour");
-				System.out.println("Enter 4 to Exit");
-				ID = keyboard.nextInt();	
 			}//end of else
 		}//end of while
 	}//end of main

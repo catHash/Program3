419b251de70d6ca28327acc3d461c1cbb089cf07
Anonymous <anonymous@Anonymouss-MacBook-Pro.local>
2017-06-26 19:01:52 -0500

    reverted

--- MPGMain.java
+++ MPGMain.java
@@ -12,17 +12,18 @@ public class MPGMain
 
 		//variables
 		double miles, gallons, distance, speed, time;
-		int ID = 0;
+		int ID;
+
+		//Menu
+		System.out.println("Enter 1 to Determine Miles-per-Gallon");
+		System.out.println("Enter 2 to Determine Distance");
+		System.out.println("Enter 3 to Determine Miles-per-Hour");
+		System.out.println("Enter 4 to Exit");
+		ID = keyboard.nextInt();
 
 		while (ID != 4)
 		{
-			//Menu
-			System.out.println("Enter 1 to Determine Miles-per-Gallon");
-			System.out.println("Enter 2 to Determine Distance");
-			System.out.println("Enter 3 to Determine Miles-per-Hour");
-			System.out.println("Enter 4 to Exit");
-			ID = keyboard.nextInt();
-			if (ID >=1 && ID <= 4)
+			if (ID >=1 && ID < 4)
 			{
 				switch(ID)
 				{
@@ -54,12 +55,16 @@ public class MPGMain
 					System.out.printf("Your MPH is: %.2f", rate.getMPH());
 					break;
 				}//end of switch
-                System.out.println();
 			}//end of of if
 			else
 			{
 				System.out.println("The value entered is not a correct entry.");
 				System.out.println("Please enter a value between 1-3 or enter 4 to exit. ");
+				System.out.println("Enter 1 to Determine Miles-per-Gallon");
+				System.out.println("Enter 2 to Determine Distance");
+				System.out.println("Enter 3 to Determine Miles-per-Hour");
+				System.out.println("Enter 4 to Exit");
+				ID = keyboard.nextInt();
 			}//end of else
 		}//end of while
 	}//end of main
